0c5e88324fdf796e0c63b1d1fe8e895a9b243ec6
David Jiang <davidjiang.haohan@gmail.com>
2019-04-06 19:46:15 -0700

    graph and matrix

--- A1/main.py
+++ A1/main.py
@@ -1,6 +1,6 @@
-import matplotlib
 from utils import load_data, create_preceptron_layer, shuffle_data, append_bias
-
+import numpy as np
+import matplotlib as plt
 
 def compute(network, x_data):
     percept_output = []
@@ -25,8 +25,23 @@ def calc_acc(predictions, labels):
         if int(predictions[i]) == int(labels[i]):
             correct += 1
     acc = float(correct/numOfPredictions)
-    print(acc)
+    print("Training Accuracy: {}".format(acc))
     return acc
+
+
+def generate_graph(acc):
+    fig, ax = plt.subplots()
+    ax.grid()
+    ax.plot(list(range(0,50)), acc)
+    ax.set(xlabel='Epochs', ylabel='Accuracy',
+           title='Accuracy per Epoch')
+    plt.show()
+
+
+def generate_matrix(predictions, y_labels):
+    matrix = np.empty([10, 10])
+    for i in range(0, len(predictions)):
+        matrix[int(predictions)][int(y_labels)] += 1
 
 def main():
     # load training data
@@ -47,25 +62,39 @@ def main():
     # epochs
     epochs = 50
 
-    # create 10 Perceptrons
-    network = create_preceptron_layer(.001)
+    learning_rates = [.001, .01, .1]
+    for rate in learning_rates:
+        print("Creating 10 Perceptrons with learning rate: {}".format(rate))
+        # Create 10 Perceptrons with learning rate rate
+        network = create_preceptron_layer(rate)
 
-    # train
-    acc = []
-    for j in range(0, epochs):
-        print("Training Epoch: {}".format(j))
-        for i in range(0, x_train.shape[0]):
-            out = compute(network, x_train[i])
-            t_values = [0] * 10
-            t_values[int(y_train[i])] = 1
-            update(network, t_values, out, x_train[i])
+        acc = []
+        for j in range(0, epochs):
+            print("Training Epoch: {}".format(j))
+            # Training
+            prediction_training = []
+            for i in range(0, x_train.shape[0]):
+                out = compute(network, x_train[i])
+                prediction_training.append(out.index(max(out)))
+                t_values = [0] * 10
+                t_values[int(y_train[i])] = 1
+                update(network, t_values, out, x_train[i])
+            accuracy_training = calc_acc(prediction_training, y_train)
 
-        prediction = []
-        for k in range(0, x_test.shape[0]):
-            percept_output = compute(network, x_test[k])
-            prediction.append(percept_output.index(max(percept_output)))
+            prediction_test = []
+            # Testing
+            for k in range(0, x_test.shape[0]):
+                percept_output = compute(network, x_test[k])
+                prediction_test.append(percept_output.index(max(percept_output)))
 
-        acc.append(calc_acc(prediction, y_test))
+            accuracy = calc_acc(prediction_test, y_test)
+            acc.append(accuracy)
+
+        # Create the graph
+        generate_graph(acc)
+
+        # Generate matrix
+        generate_matrix(prediction_test, y_test)
 
 
 if __name__ == '__main__':
